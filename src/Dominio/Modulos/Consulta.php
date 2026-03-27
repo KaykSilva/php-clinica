3d78e006fd7b9c439b7a013ade54ed5372d0835d
KaykSilva <kayksilva489@gmail.com>
2026-03-27 14:24:15 -0300

    WP: Consulta dominio

--- src/Dominio/Modulos/Consulta.php
+++ src/Dominio/Modulos/Consulta.php
@@ -2,17 +2,45 @@
 
 namespace Luizlins\Projeto01\Dominio\Modulos;
 
-use Luizlins\Projeto01\Dominio\Modulos\Medico;
-use Luizlins\Projeto01\Dominio\Modulos\Paciente;
 use DateTimeImmutable;
 
-class Consulta {
-
-    function __construct(
+class Consulta
+{
+    public function __construct(
+        private ?int $id,
         private Medico $medico,
         private Paciente $paciente,
         private DateTimeImmutable $data,
         private float $valor
     ) {}
 
+    public function recuperarId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function definirId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function recuperarMedico(): Medico
+    {
+        return $this->medico;
+    }
+
+    public function recuperarPaciente(): Paciente
+    {
+        return $this->paciente;
+    }
+
+    public function recuperarData(): DateTimeImmutable
+    {
+        return $this->data;
+    }
+
+    public function recuperarValor(): float
+    {
+        return $this->valor;
+    }
 }
